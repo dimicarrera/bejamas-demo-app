86a5f71eaa61928bf4b931c4a6002b2938fc90ca
Dmitri Carrera <dmitri.sincariuc@post.cz>
2022-11-15 21:46:50 +0100

    Delete context

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { DataState } from "./context/DataContext";
-
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<DataState>
-			<App />
-		</DataState>
+		<App />
 	</React.StrictMode>
 );
